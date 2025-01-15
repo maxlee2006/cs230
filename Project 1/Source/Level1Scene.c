70168c974026d8d83da25864c7f8007a8fbc0a52
Max Lee <max.lee@digipen.edu>
2025-01-14 20:47:01 -0800

    aeod

--- Project 1/Source/Level1Scene.c
+++ Project 1/Source/Level1Scene.c
@@ -1,6 +1,6 @@
 //------------------------------------------------------------------------------
 //
-// File Name:	Stub.c
+// File Name:	Level1Scene
 // Author(s):	Max Lee
 // Project:		Project 0
 // Course:		CS230S25
@@ -10,7 +10,10 @@
 //------------------------------------------------------------------------------
 
 #include "stdafx.h"
-#include "Stub.h"
+
+#include "Scene.h"
+#include "SceneSystem.h"
+#include "Level1Scene.h"
 
 //------------------------------------------------------------------------------
 // Private Constants:
@@ -19,6 +22,16 @@
 //------------------------------------------------------------------------------
 // Private Structures:
 //------------------------------------------------------------------------------
+
+typedef struct Level1Scene
+{
+	// WARNING: The base class must always be included first.
+	Scene	base;
+
+	// Add any scene-specific variables second.
+	int numLives;
+
+} Level1Scene;
 
 //------------------------------------------------------------------------------
 // Public Variables:
@@ -32,14 +45,77 @@
 // Private Function Declarations:
 //------------------------------------------------------------------------------
 
+static void Level1SceneLoad(void);
+static void Level1SceneInit(void);
+static void Level1SceneUpdate(float dt);
+static void Level1SceneExit(void);
+static void Level1SceneUnload(void);
+static void Level1SceneRender(void);
+
+//------------------------------------------------------------------------------
+// Instance Variable:
+//------------------------------------------------------------------------------
+
+static Level1Scene instance =
+{
+	// Initialize the base structure:
+	{ "Level1", Level1SceneLoad, Level1SceneInit, Level1SceneUpdate, Level1SceneRender, Level1SceneExit, Level1SceneUnload },
+
+	// Initialize any scene-specific variables:
+	
+};
+
 //------------------------------------------------------------------------------
 // Public Functions:
 //------------------------------------------------------------------------------
 
-// Initialize the ...
-
+// Get the instance of the Stub Scene.
+// Returns:
+//	 Pointer to the base Scene structure for this derived Scene.
+const Scene* Level1SceneGetInstance(void)
+{
+	return &(instance.base);
+}
 
 //------------------------------------------------------------------------------
 // Private Functions:
 //------------------------------------------------------------------------------
+
+// Load any resources used by the scene.
+static void Level1SceneLoad(void)
+{
+}
+
+// Initialize the entities and variables used by the scene.
+static void Level1SceneInit()
+{
+}
+
+// Update the the variables used by the scene.
+// Params:
+//	 dt = Change in time (in seconds) since the last game loop.
+static void Level1SceneUpdate(float dt)
+{
+	// Tell the compiler that the 'dt' variable is unused.
+	UNREFERENCED_PARAMETER(dt);
+
+	// NOTE: This call causes the engine to exit immediately.  Make sure to remove
+	//   it when you are ready to test out a new scene.
+	SceneSystemSetNext(NULL);
+}
+
+// Render any objects associated with the scene.
+void Level1SceneRender(void)
+{
+}
+
+// Free any objects associated with the scene.
+static void Level1SceneExit()
+{
+}
+
+// Unload any resources used by the scene.
+static void Level1SceneUnload(void)
+{
+}
 
